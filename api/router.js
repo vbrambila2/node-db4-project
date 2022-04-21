722115f1b54952d52a5e1e9fc420452725483841
Victor Brambila <victorbrambila22@gmail.com>
2022-04-21 15:29:09 -0700

    postman loads correct syntax

--- api/router.js
+++ api/router.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const Model = require('./model');
 
 const router = express.Router();
+
+router.get('/recipes', (req, res) => {
+    Model.getRecipes()
+        .then(rec => {
+            res.json(rec)
+        })
+        .catch(() => {
+            res.status(500).json({ message: '500 error getRecipes' })
+        })
+})
+
+router.get('/recipes/:recipe_id', (req, res) => {
+    console.log(req.params.recipe_id);
+    Model.getRecipeById(req.params.recipe_id)
+        .then(rec => {
+            res.json(rec)
+        })
+        .catch(() => {
+            res.status(500).json({ message: '500 error getRecipeById' })
+        })
+})
 
 module.exports = router;
--- data/seeds/04-steps.js
+++ data/seeds/04-steps.js
@@ -1,9 +1,13 @@
+const steps = [
+  {recipe_id: 1, step_number: 1, ingredient_id: 1, instructions: 'add cheese'},
+  {recipe_id: 1, step_number: 2, ingredient_id: 2, instructions: 'melt cheese on bread'},
+  {recipe_id: 2, step_number: 1, ingredient_id: 3, instructions: 'add bacon'},
+  {recipe_id: 2, step_number: 2, ingredient_id: 4, instructions: 'add lettuce'},
+  {recipe_id: 2, step_number: 3, ingredient_id: 5, instructions: 'add tomato'} ,
+]
+
+exports.steps = steps
+
 exports.seed = function(knex) {
-  return knex('steps').insert([
-    {step_number: 1, instructions: 'add cheese', recipe_id: 1, ingredient_id: 1},
-    {step_number: 2, instructions: 'melt cheese on bread', recipe_id: 1, ingredient_id: 2},
-    {step_number: 1, instructions: 'add bacon', recipe_id: 2, ingredient_id: 3},
-    {step_number: 2, instructions: 'add lettuce', recipe_id: 2, ingredient_id: 4},
-    {step_number: 3, instructions: 'add tomato', recipe_id: 2, ingredient_id: 5} ,
-  ]);
+  return knex('steps').insert(steps);
 };
